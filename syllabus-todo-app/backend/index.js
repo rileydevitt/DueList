c268781da547d0594bf7647a8348265bcde915e2
Riley Devitt <118942813+rileydevitt@users.noreply.github.com>
2025-09-18 13:11:36 -0300

    Update index.js

--- syllabus-todo-app/backend/index.js
+++ syllabus-todo-app/backend/index.js
@@ -15,6 +15,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.use(express.json());
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });
 
 // File upload configuration
 const upload = multer({
